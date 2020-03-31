c343e7311f2d57b40796e2640314b55b87deb52c
CCRRX <jan.koppatscheck@gmail.com>
2020-03-31 19:06:17 +0200

    neue Datei:     ssl.sh

---
+++ ssl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+wget -q http://stream.sunshine-live.de/
+
+n=1
+
+cat index.html | grep 'mp3-192/stream.sunshine-live.de/"' | cut -c 59- | rev | cut -c40- | rev | cut -f1 -d'"' > ssl-streams
+l=$(wc -l < ssl-streams)
+
+echo "[playlist]"
+echo "NumberOfEntries=$l"
+echo ""
+
+while read p; do
+  #var=${p%-???}
+  #var=${p::${#p}31}
+  var=${p::-33}
+  var2=${var:31}
+  echo "File$n=$p"
+  echo "Title$n=$var2"
+  echo "Length1=-1"
+  echo ""
+  n=$((n + 1))
+done < ssl-streams
+
+rm ssl-streams
+rm index.html
